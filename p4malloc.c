ca9a8ee0115cbf40c1b52b1e8cd6cc0cbbb189ab
nerivmel <manuel.calle@udea.edu.co>
2019-08-20 17:10:41 -0500

    Practica lab

---
+++ p4malloc.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *m = malloc(5 * sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        *(m + i) = 0;
+        printf("%d\n", *(m + i));
+    }
+}
